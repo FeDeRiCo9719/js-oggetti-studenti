5cb2453d5e89e57c08c867e3f02a0c32e8349b52
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2021-09-07 18:42:59 +0200

    array loop

--- Js/script.js
+++ Js/script.js
@@ -15,13 +15,39 @@ var studente = {
 
 // 2. Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
 for ( var key in studente ) {
-    console.log( [key] );
-    document.getElementById('student').innerHTML += `${studente[key]} <br>`; 
+    console.log( studente[key] );
+    document.getElementById('student').innerHTML += `${key}: ${studente[key]} <br>`; 
 }
 
 // 3. Creare un array di oggetti di studenti.
+var studenti = [
+    {
+        'nome': 'Mario',
+        'cognome': 'Verdi',
+        'età': '23',
+    },
+
+    {
+        'nome': 'Alex',
+        'cognome': 'Gialli',
+        'età': '24',
+    },
+
+    {
+        'nome': 'Sandro',
+        'cognome': 'Neri',
+        'età': '26',
+    }
+]
 
 // 4. Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+
+for (var i = 0; i < studenti.length; i++) {
+
+    document.getElementById('students').innerHTML += `${studenti[i]["nome"]}  ${studenti[i]["cognome"]}  ${studenti[i]["età"]} <br>`;
+
+}
+
 
 // 5. Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
